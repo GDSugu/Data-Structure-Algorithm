17f8571881fc355ebfd5f0d048195d43aa0d1173
HEXAMAP\10196 <sugumar@hexamapsolutions.com>
2025-03-17 10:39:41 +0530

    added task folder changes

--- TaskList/2025/March/week_1/Answers/easy.js
+++ TaskList/2025/March/week_1/Answers/easy.js
@@ -22,3 +22,28 @@ var removeDuplicates = function(nums) {
 };
 
 
+/**************************************** BEST TIME TO BUY AND SELL STOCK  ********************************
+ * LEETCODE 2: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+*/
+
+// SOLUTION: USING SINGLE PASS METHOD
+
+var maxProfit = function(prices) {
+    let minPrice = Infinity;
+    let maxProfit = 0;
+
+    for (let price of prices) {
+        if (price < minPrice) {
+            minPrice = price; // Update the lowest price found so far
+        } else {
+            maxProfit = Math.max(maxProfit, price - minPrice); // Calculate max profit
+        }
+    }
+
+    return maxProfit;
+};
+
+
+
+
+
