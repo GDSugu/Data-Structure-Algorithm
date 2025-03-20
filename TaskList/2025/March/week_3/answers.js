fcb9ce4ca82593c546f910fbb042c8da6685ff28
HEXAMAP\10196 <sugumar@hexamapsolutions.com>
2025-03-20 11:00:15 +0530

    added kadanes algorithm folder

--- TaskList/2025/March/week_3/answers.js
+++ TaskList/2025/March/week_3/answers.js
@@ -475,3 +475,31 @@ var maxConsecutiveAnswers = function(answerKey, k) {
 
     return Math.max(maxConsecutiveChar('T'), maxConsecutiveChar('F'));
 };
+
+
+/************************************** MAXIMUM SUM SUBARRAY  *************************************************
+ * Leetcode 14: https://leetcode.com/problems/maximum-subarray/
+*/
+
+// SOLUTION: KADANE'S ALGORITHM -> O(N)
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function maxSubArray(nums) {
+    let maxSum = nums[0];  // Stores the maximum sum found
+    let currentSum = 0;    // Tracks the current subarray sum
+
+    for (let num of nums) {
+        currentSum = Math.max(num, currentSum + num); // Extend or restart subarray
+        maxSum = Math.max(maxSum, currentSum); // Update max sum if needed
+    }
+
+    return maxSum;
+}
+
+// Test Cases
+console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4])); // Output: 6
+console.log(maxSubArray([1])); // Output: 1
+console.log(maxSubArray([5,4,-1,7,8])); // Output: 23
